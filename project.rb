ca4cf2e29e74bc0f7ca8a4545bbeef36700a9649
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-10 21:26:14 -0700

    added initializer

--- project.rb
+++ project.rb
@@ -1,6 +1,11 @@
 class Project
     attr_accessor :name, :description
 
+    def initialize (name, desc)
+        @name = name
+        @description = desc
+    end
+
     def elevator_pitch
         return "#{@name}, #{@description}"
     end
